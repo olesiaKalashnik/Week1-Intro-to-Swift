5c98d28da32003c6f05fdf06a6ff0d168ed67773
Olesia Kalashnik <seventhform@gmail.com>
2016-06-06 14:56:36 -0700

    Clean up code in Homework1

---
+++ class-1/OlesiaKalashnik/Week1Homework1.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+//Write a function that takes in a string, checks if it can be converted to a number and if possible, returns a number
+func stringToInt(string: String) -> Int? {
+    return Int(string)
+}
+
+func stringToIntExplicit(string: String) -> Int? {
+    if let integer = Int(string) {
+        return integer
+    } else {
+        return nil
+    }
+}
+
+func stringToIntWithFormatter(string: String) -> Int? {
+    return NSNumberFormatter().numberFromString(string) as? Int
+}
+
+//Write a function that takes in a tuple (code, message) and returns a string by using interpolation
+func makeErrorMessage(errorTuple: (code: Int, message: String)) -> String {
+    return "The status is \(errorTuple.code), \(errorTuple.message)"
+}
+
+//Write a function that takes in a string, and returns a message: “String contains n characters”
+func countCharactersInString(string: String) -> String {
+    return "String contains \(string.characters.count)"
+}
+
+//Write a function that takes in a string and reverses it.
+func reverseString(string: String) -> String {
+    var stringAsArray = string.characters.map {$0}
+    
+    let stringLength = stringAsArray.count
+    var indexEnd = stringLength - 1
+    while indexEnd > (stringLength - 1)/2 {
+        let currEndElement = stringAsArray[indexEnd]
+        stringAsArray[indexEnd] = stringAsArray[(stringLength - 1) - indexEnd]
+        stringAsArray[(stringLength - 1) - indexEnd] = currEndElement
+        indexEnd -= 1
+    }
+    return String(stringAsArray)
+}
+
+//Declare a variable that’s function. Implement function to take in a string (name) and return greeting message.
+var greetingMessage : (String) -> String = { (name) in return "Hello, \(name)!" }
+
